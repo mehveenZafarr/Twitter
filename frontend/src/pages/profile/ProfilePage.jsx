44a5c43965fbe7d39f3200453107866938ed3151
mehveenzafar <121675157+mehveenzafar@users.noreply.github.com>
2024-07-27 18:24:01 +0500

    Update user profile modal in editProfileModal

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -6,13 +6,15 @@ import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkele
 import EditProfileModal from "./EditProfileModal";
 
 import { POSTS } from "../../utils/db/dummy";
+import useFollow from '../../components/hooks/useFollow.jsx';
 
 import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { formatMemberSinceDate } from "../../utils/date";
+import toast from "react-hot-toast";
 
 const ProfilePage = () => {
 	const [coverImg, setCoverImg] = useState(null);
@@ -24,8 +26,10 @@ const ProfilePage = () => {
 
 	const {username} = useParams();
 
+	const {follow, isPending} = useFollow();
+	const queryClient = useQueryClient();
 	// const isLoading = false;
-	const isMyProfile = true;
+	
 
 	const {data: user, isLoading, refetch, isRefetching} = useQuery({
 		queryKey: ["userProfile"],
@@ -42,7 +46,11 @@ const ProfilePage = () => {
 			}
 		},
 	});
+	
 	const memberSinceDate = formatMemberSinceDate(user?.createdAt);
+	const {data:authUser} = useQuery({queryKey: ["authUser"]});
+
+	const isMyProfile = authUser._id === user?._id;
 
 	const handleImgChange = (e, state) => {
 		const file = e.target.files[0];
@@ -55,6 +63,38 @@ const ProfilePage = () => {
 			reader.readAsDataURL(file);
 		}
 	};
+
+	
+	const {mutate:updateProfile, isPending: isUpdatingProfile} = useMutation({
+		mutationFn: async () => {
+			const res = await fetch("/api/users/update", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					coverImg,
+					profileImg,
+				})
+			});
+			const data = await res.json();
+			if(!res.ok) {
+				throw new Error(data.error || "Something went wrong!");
+			}
+		},
+		onSuccess: () => {
+			toast.success("Profile updated successfully!");
+			Promise.all([
+				queryClient.invalidateQueries({queryKey: ["authUser"]}),
+				queryClient.invalidateQueries({queryKey: ["userProfile"]})
+			]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		}
+	});
+
+	const amIFollowing = authUser?.following.includes(user?._id);
 
 	useEffect(() => {
 		refetch();
@@ -127,21 +167,26 @@ const ProfilePage = () => {
 								</div>
 							</div>
 							<div className='flex justify-end px-4 mt-5'>
-								{isMyProfile && <EditProfileModal />}
+								{isMyProfile && <EditProfileModal authUser={authUser}/>}
 								{!isMyProfile && (
 									<button
 										className='btn btn-outline rounded-full btn-sm'
-										onClick={() => alert("Followed successfully")}
+										onClick={() => follow(user?._id)}
 									>
-										Follow
+										{/* {isPending && "Loading..."}
+										{!isPending && amIFollowing && "UnFollow"}
+										{!isPending && !amIFollowing && "Follow"} */}
+										{isPending && "Loading..."}
+										{!isPending && amIFollowing && "Unfollow"}
+										{!isPending && !amIFollowing && "Follow"}
 									</button>
 								)}
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => alert("Profile updated successfully")}
+										onClick={() => updateProfile()}
 									>
-										Update
+										{isUpdatingProfile? "Updating..." : "Update"}
 									</button>
 								)}
 							</div>
@@ -159,12 +204,12 @@ const ProfilePage = () => {
 											<>
 												<FaLink className='w-3 h-3 text-slate-500' />
 												<a
-													href='https://youtube.com/@asaprogrammer_'
+													href= {user?.link}
 													target='_blank'
 													rel='noreferrer'
 													className='text-sm text-blue-500 hover:underline'
 												>
-													youtube.com/@asaprogrammer_
+													{user?.link}
 												</a>
 											</>
 										</div>
