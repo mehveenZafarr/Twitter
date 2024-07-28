200fe922d246a3f17007966c6b4835dcb57e84a8
mehveenzafar <121675157+mehveenzafar@users.noreply.github.com>
2024-07-28 12:30:13 +0500

    Prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRoutes from './routes/userRoutes.js';
 import { v2 as cloudinary } from "cloudinary";
 import postRoutes from './routes/postRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
+import path from "path";
 // import bodyParser from 'body-parser';
 
 
@@ -18,6 +19,7 @@ cloudinary.config({
 });
 const app = express();
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
@@ -30,6 +32,14 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
